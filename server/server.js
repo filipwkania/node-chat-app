b0e48628ebef6a81ae790856ae8f748608cb0a89
livarion <livarion@gmail.com>
2017-07-24 23:20:22 +0200

    babel and socket broadcast

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const socketIO = require('socket.io');
 const http = require('http');
 
-
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -14,6 +13,23 @@ app.use(express.static(publicPath));
 
 io.on( 'connection', (socket) => {
   console.log('New user connected');
+  
+  socket.emit('newMessage', {
+    from:"Admin",
+    text:"Welcome to the chat room!"
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from:"Admin",
+    text:"New user joined the chat"
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('New message from', message.from);
+    message.createdAt = (+ new Date());
+    socket.broadcast.emit('newMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log('User disconnected');
   });
